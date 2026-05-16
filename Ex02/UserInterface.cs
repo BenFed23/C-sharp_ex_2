5cde3b6ad69e9757123a408e46a781a1ffbd9108
Ben Fediuck <benfe@mta.ac.il>
2026-05-16 19:14:09 +0300

    bord class some of bord logic methods

---
+++ Ex02/UserInterface.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Ex02
+{
+    internal class UserInterface
+    {
+        public static void DrawBoard(char[,] i_gameBoard) 
+        {
+            //darw bord like in the picture
+            //fill the board
+            foreach (char item in i_gameBoard)
+            {
+                Console.WriteLine(item); 
+            }
+        }
+        public static void ShowMessage(string message) 
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
