2ce0982ce092d8608943a0c7a803d753120829d2
mayageva2 <mayageva7@gmail.com>
2026-05-17 12:58:50 +0300

    creating game run loop - needs testing

--- Ex02/Game.cs
+++ Ex02/Game.cs
@@ -5,23 +5,90 @@ namespace Ex02
 {
     internal class Game
     {
-        TicTacToeBoard board;
-        bool m_twoPlayers;
+        private readonly UserInterface m_UserInterface;
+        private GameEngine m_Engine;
+        private TicTacToeBoard m_Board;
+        private Player m_Player1;
+        private Player m_Player2;
+        private Player m_CurrentPlayer;
+        private bool m_IsAgainstComputer;
+
         public Game() 
         {
-            //Maya logic here..
-        }
-        public  void Run() 
-        {
-            if (this.m_twoPlayers)
-            {
-                //Game.Rungamewithhuman();
-            }
-            else 
-            {
-                //Game.Rungamewithcomputer();
-            }
+            m_UserInterface = new UserInterface();
+            m_Player1 = new Player('X', "Player 1");
+            m_Player2 = new Player('O', "Player 2");
 
+            m_CurrentPlayer = m_Player1;
+        }
+        public void Run() 
+        {
+            m_UserInterface.GetGameSettingsFromUser(out int boardSize, out m_IsAgainstComputer);
+            m_Board = new TicTacToeBoard(boardSize, boardSize);
+            m_Engine = new GameEngine();
+            runGameLoop();
+        }
+
+        private void runGameLoop()
+        {
+            bool isGameOver = false;
+
+            while (!isGameOver)
+            {
+                UserInterface.DrawBoard(m_Board);
+                UserInterface.ShowMessage($"Player {m_CurrentPlayer.Name}'s turn.");
+
+                int boardRow = 0;
+                int boardColumn = 0;
+                bool wasQKeyPressed = false;
+
+                if (m_CurrentPlayer == m_Player2 && m_IsAgainstComputer)
+                {
+                    UserInterface.ShowMessage("Computer is thinking...");
+                    //computer move logic 
+                }
+                else
+                {
+                    m_UserInterface.GetValidNextMoveFromUser(m_Board.GetLength(), out boardRow, out boardColumn, out wasQKeyPressed);
+                }
+
+                if (wasQKeyPressed)
+                {
+                    UserInterface.ShowMessage("Game stopped by User");
+                    break;
+                }
+
+                int boardRowIndex = boardRow - 1;
+                int boardColIndex = boardColumn - 1;
+                bool isMoveSuccessful = m_Board.FillCell(boardRowIndex, boardColIndex, m_CurrentPlayer.Sign);
+                while (!isMoveSuccessful)
+                {
+                    UserInterface.ShowMessage("This cell is already full! Choose another one.");
+                }
+
+                if (m_Engine.IsFullRowColumnOrDiagonalInBoard(m_Board))
+                {
+                    UserInterface.DrawBoard(m_Board);
+                    m_CurrentPlayer.Score++;
+                    UserInterface.ShowMessage($"Player {m_CurrentPlayer.Sign} Won! Score: {m_CurrentPlayer.Score}");
+                    isGameOver = true;
+                }
+                else if (m_Engine.isFullBoard(m_Board))
+                {
+                    UserInterface.DrawBoard(m_Board);
+                    UserInterface.ShowMessage("It's a tie! The board is full.");
+                    isGameOver = true;
+                }
+                else
+                {
+                    switchPlayer();
+                }
+            }
+        }
+
+        private void switchPlayer()
+        {
+            m_CurrentPlayer = (m_CurrentPlayer == m_Player1) ? m_Player2 : m_Player1;
         }
     }
 }
--- Ex02/Player.cs
+++ Ex02/Player.cs
@@ -6,11 +6,34 @@ namespace Ex02
     internal class Player
     {
         private int m_score;
-        private char m_sighn;
-        public Player(char i_sighn) 
+        private char m_sign;
+        private string m_name;
+
+        public Player(char i_sign, string i_name) 
         {
-            m_sighn = i_sighn;
+            m_sign = i_sign;
+            m_name
             m_score = 0;
+        }
+
+        public char Sign
+        {
+            get 
+            { 
+                return m_sign; 
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return m_score;
+            }
+            set
+            {
+                m_score = value;
+            }
         }
 
     }
